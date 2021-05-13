668c7293d4cf9fdf8ca5438adcde22564dcf1472
amrit sharma <sharmaamrit.a5432@gmail.com>
2021-05-14 01:22:19 +0545

    fixed gallery bug

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose');
 mongoose
 
   // .connect('mongodb://localhost:27017/herald', {
-  .connect('mongodb+srv://amrit:amrit123@cluster0.9tmuy.mongodb.net/test', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    'mongodb+srv://Amrit:awymNmlRx19VrXEJ@cluster0.xlzqn.mongodb.net/test?authSource=admin&replicaSet=atlas-7sc1fb-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
   .then(() => console.log('DB connection successful!'))
   .catch((err) => {
     console.log('failed to connect with database');
